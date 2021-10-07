9552b6fd144cc71e5964aafe526bbc5fa230f3ca
0x01code <donghappy123@gmail.com>
2021-10-07 09:26:59 +0700

    Add project files.

---
+++ QRCode/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics g = Graphics.FromImage(bitmap);
+                g.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+                pictureBox1.Image = bitmap;
+
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)pictureBox1.Image);
+                if(result != null)
+                {
+                    textBox1.Text = result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
+        }
+    }
+}
